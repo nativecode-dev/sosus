023f1d155ff72dc7802e091e123443231cce6f22
NativeCode <opensource@nativecode.com>
2020-01-14 00:45:14 -0500

    chore: update plex options

--- packages/core-process/src/RegisterDependencies.ts
+++ packages/core-process/src/RegisterDependencies.ts
@@ -1,7 +1,7 @@
-import { PlexCloud } from '@nativecode/plex'
 import { RadarrClient } from '@nativecode/radarr'
 import { SonarrClient } from '@nativecode/sonarr'
 import { DependencyContainer, LoggerType, Lincoln } from '@sosus/core'
+import { PlexCloud, PlexClient, PlexClientOptions, PlexCloudOptions } from '@nativecode/plex'
 
 import { ProcessConfigType, ProcessConfig } from './ProcessConfig'
 
@@ -22,17 +22,33 @@ export function registerCommands(container: DependencyContainer) {
   container.register<CommandInstance>(CommandType, SonarrImport)
   container.register<CommandInstance>(CommandType, SonarrUnmonitor)
 
+  container.register<PlexClient>(PlexClient, {
+    useFactory: container => {
+      const config = container.resolve<ProcessConfig>(ProcessConfigType)
+      const logger = container.resolve<Lincoln>(LoggerType)
+
+      const options: Partial<PlexClientOptions> = {
+        auth: {
+          username: config.plex.auth?.username,
+          password: config.plex.auth?.password,
+        },
+        host: config.plex.host,
+      }
+
+      return new PlexClient(options, logger)
+    },
+  })
+
   container.register<PlexCloud>(PlexCloud, {
     useFactory: container => {
       const config = container.resolve<ProcessConfig>(ProcessConfigType)
       const logger = container.resolve<Lincoln>(LoggerType)
 
-      const options = {
+      const options: Partial<PlexCloudOptions> = {
         auth: {
           username: config.plex.auth?.username,
           password: config.plex.auth?.password,
         },
-        host: config.radarr.host,
       }
 
       return new PlexCloud(options, logger)
